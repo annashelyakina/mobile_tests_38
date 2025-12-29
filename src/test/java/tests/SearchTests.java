0da0e7a4d41a3051a820397c1a28edf321c7bbd9
annashelyakina <anna.shelyakina82@gmail.com>
2025-12-29 20:19:22 +0300

    updated SearchTests

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -8,6 +9,7 @@ import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.*;
 import static io.qameta.allure.Allure.step;
 
+@Tag("mobile_tests")
 public class SearchTests extends TestBase{
 
     @Test
